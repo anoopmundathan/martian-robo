fee99bbdb0579c3d1c1aaf7fea662f23bc2ecd91
Anoop Mundathan <anoop.mundathan@gmail.com>
2018-02-01 05:02:14 +0000

    Create robot objects from input

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
 const Grid = require('./grid')
 const Robot = require('./robot')
 
-function martianRobo(gridInput, robots) {
+function martianRobo(gridInput, robotsInput) {
     
     // 1. TODO: Validate gridInput
 
-    // 2. TODO: Validate robots
-
-    // 3. TODO: Create grid object
+    // 2. Create grid object
     const gridData = gridInput.split(' ');
     const grid = new Grid(gridData[0], gridData[1]);
     
-    // 4. TODO: Create robot object
-    
+    const robots = robotsInput.map((input) => {
+        const data = input.split(' ');
+        
+        // 3.TODO: Validate robots object
+
+        // 4. Create each robot object
+        return new Robot(data[0], data[1], data[2], data[3].split(''))
+    })
+
     // TODO: Iterate robots object array
 
     // TODO: Iterate robots commands array
